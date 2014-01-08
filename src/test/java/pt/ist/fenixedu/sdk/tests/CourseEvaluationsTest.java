aaf2d0e05d8cbf630049da96ecadd9b82e7c353a
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-08 17:03:06 +0000

    Changed test according to the new implementation of getCourseEvaluations in client class

--- src/test/java/pt/ist/fenixedu/sdk/tests/CourseEvaluationsTest.java
+++ src/test/java/pt/ist/fenixedu/sdk/tests/CourseEvaluationsTest.java
@@ -1,24 +1,27 @@
 package pt.ist.fenixedu.sdk.tests;
 
+import java.util.List;
+
 import pt.ist.fenixedu.sdk.beans.publico.FenixCourseEvaluation;
 
 public class CourseEvaluationsTest extends PersonCoursesTest {
 
-	public CourseEvaluationsTest() {
-	}
+    public CourseEvaluationsTest() {
+    }
 
-	public CourseEvaluationsTest(String name) {
-		super(name);
-	}
+    public CourseEvaluationsTest(String name) {
+        super(name);
+    }
 
-	public void testNotNull() {
-		super.testNotNull();
-		String courseId = getCourses().getEnrolments().get(0).getId();
-		
-		FenixCourseEvaluation[] evaluations = getClient().getCourseEvaluations(courseId);
-		
-		for(FenixCourseEvaluation evaluation : evaluations) {
-			assertNotNull("Name is null", evaluation.getName());
-		}
-	}
+    @Override
+    public void testNotNull() {
+        super.testNotNull();
+        String courseId = getCourses().getEnrolments().get(0).getId();
+
+        List<FenixCourseEvaluation> evaluations = getClient().getCourseEvaluations(courseId);
+
+        for (FenixCourseEvaluation evaluation : evaluations) {
+            assertNotNull("Name is null", evaluation.getName());
+        }
+    }
 }
