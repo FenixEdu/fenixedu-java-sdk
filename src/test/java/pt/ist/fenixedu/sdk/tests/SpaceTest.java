8285cd152defdc5a1840221b91e7774630889556
Samuel Coelho <samuelfcmc@gmail.com>
2014-01-08 14:35:14 +0000

    Added tests to get spaces and course evaluations

---
+++ src/test/java/pt/ist/fenixedu/sdk/tests/SpaceTest.java
@@ -0,0 +1,48 @@
+package pt.ist.fenixedu.sdk.tests;
+
+import pt.ist.fenixedu.sdk.beans.publico.FenixSpace;
+
+public class SpaceTest extends FenixEduTestCase {
+	private final String campusId = "2465311230081";
+	private final String buildingId = "2972117371245";
+	private final String floorId = "";
+	private final String roomId = "";
+	private final String day = "";
+
+	public SpaceTest() {
+	}
+
+	public SpaceTest(String name) {
+		super(name);
+	}
+
+	public void testNotNull() {
+		 assertNotNull(getClient().getSpace(campusId, day));
+		 
+		 assertNotNull(getClient().getSpace(buildingId, day));
+	}
+	
+	public void assertNotNull(FenixSpace.Campus campus) {
+		assertNotNull("Campus name is null", campus.getName());
+		assertNotNull("Campus id is null", campus.getId());
+		assertNotNull("Campus type is null", campus.getType());
+		assertNotNull("Campus contained spaces is null", campus.getContainedSpaces());
+		//assertNotNull("Campus parent space is null", campus.getParentSpace());
+	}
+	
+	//public void assertNotNull(FenixSpace space) {
+		//fail("Didn't call the specific asserts " + space.getName());
+//	}
+	
+	public void assertNotNull(FenixSpace.Building building) {
+		
+	}
+	
+	public void assertNotNull(FenixSpace.Floor floor) {
+		
+	}
+	
+	public void assertNotNull(FenixSpace.Room room) {
+		
+	}
+}
