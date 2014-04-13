70c1b934431a5b2131e9c3b5e22b5ede33e7ba0b
Afonso Garcia <afonsogarcia@icloud.com>
2014-04-13 19:14:41 +0100

    Modified GetPersonCalendarClassesAsyncTask and GetPersonCoursesAsyncTask to call the correct endpoints

--- feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetPersonCalendarClassesAsyncTask.java
+++ feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetPersonCalendarClassesAsyncTask.java
@@ -2,25 +2,21 @@ package org.fenixedu.sdk.android;
 
 import org.fenixedu.sdk.Authorization;
 import org.fenixedu.sdk.FenixEduClient;
+import org.fenixedu.sdk.domain.CalendarFormat;
 
 import com.google.gson.JsonObject;
 
 /**
- * This endpoint returns the user's class information.
- * This information can be retrieved both in iCalendar and JSON formats.
+ * This endpoint returns the user's course information.
  */
-public class GetPersonCalendarClassesAsyncTask extends AuthorizedFenixEduAsyncTask<String, JsonObject> {
+public class GetPersonCalendarClassesAsyncTask extends AuthorizedFenixEduAsyncTask<CalendarFormat, JsonObject> {
 
     public GetPersonCalendarClassesAsyncTask(FenixEduClient client, Authorization authorization) {
         super(client, authorization);
     }
 
     @Override
-    protected JsonObject doInBackground(String... params) {
-        if (params.length == 0) {
-            return getClient().getPersonCourses(getAuthorization());
-        } else {
-            return getClient().getPersonCourses(getAuthorization(), params[0]);
-        }
+    protected JsonObject doInBackground(CalendarFormat... params) {
+        return getClient().getPersonCalendarClasses(getAuthorization(), params[0]);
     }
 }
--- feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetPersonCoursesAsyncTask.java
+++ feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetPersonCoursesAsyncTask.java
@@ -2,21 +2,25 @@ package org.fenixedu.sdk.android;
 
 import org.fenixedu.sdk.Authorization;
 import org.fenixedu.sdk.FenixEduClient;
-import org.fenixedu.sdk.domain.CalendarFormat;
 
 import com.google.gson.JsonObject;
 
 /**
- * This endpoint returns the user's course information.
+ * This endpoint returns the user's class information.
+ * This information can be retrieved both in iCalendar and JSON formats.
  */
-public class GetPersonCoursesAsyncTask extends AuthorizedFenixEduAsyncTask<CalendarFormat, JsonObject> {
+public class GetPersonCoursesAsyncTask extends AuthorizedFenixEduAsyncTask<String, JsonObject> {
 
     public GetPersonCoursesAsyncTask(FenixEduClient client, Authorization authorization) {
         super(client, authorization);
     }
 
     @Override
-    protected JsonObject doInBackground(CalendarFormat... params) {
-        return getClient().getPersonCalendarClasses(getAuthorization(), params[0]);
+    protected JsonObject doInBackground(String... params) {
+        if (params.length == 0) {
+            return getClient().getPersonCourses(getAuthorization());
+        } else {
+            return getClient().getPersonCourses(getAuthorization(), params[0]);
+        }
     }
 }
