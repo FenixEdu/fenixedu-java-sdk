661b9af0c9bd7ba7529cd46799f3a90cc0b1fc9b
Ricardo Barata <ricardo.barata@gmail.com>
2014-07-30 19:38:43 +0100

    Fixed return type in /courses/id/groups.

--- feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetCourseGroupsAsyncTask.java
+++ feaf4j-android/src/main/java/org/fenixedu/sdk/android/GetCourseGroupsAsyncTask.java
@@ -2,7 +2,7 @@ package org.fenixedu.sdk.android;
 
 import org.fenixedu.sdk.FenixEduClient;
 
-import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
 
 /**
  * Groups are used in courses for a wide range of purposes. The most typical are for creating teams of students for laboratories
@@ -10,15 +10,15 @@ import com.google.gson.JsonObject;
  * Some groups are shared among different courses. The enrolment of student groups may be atomic or individual, and may be
  * restricted to an enrolment period.
  */
-public class GetCourseGroupsAsyncTask extends FenixEduAsyncTask<String, JsonObject> {
+public class GetCourseGroupsAsyncTask extends FenixEduAsyncTask<String, JsonArray> {
 
     public GetCourseGroupsAsyncTask(FenixEduClient client) {
         super(client);
     }
 
     @Override
-    protected JsonObject executeInBackground(String... params) {
-        return getClient().publicScope().getCourseStudents(params[0]);
+    protected JsonArray executeInBackground(String... params) {
+        return getClient().publicScope().getCourseGroups(params[0]);
     }
 
 }
